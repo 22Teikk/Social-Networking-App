a021df2f5ddfbf6a0b05324476e8cc541b4d8385
22Teikk <nguyenkiet228@gmail.com>
2023-11-01 07:28:34 +0700

    Done base UI

--- app/src/main/java/com/example/chatapp/chat/Chat_Activity.kt
+++ app/src/main/java/com/example/chatapp/chat/Chat_Activity.kt
@@ -1,4 +1,4 @@
-package com.example.chatapp.newsfeed
+package com.example.chatapp.chat
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,26 +7,26 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.chatapp.R
-import com.example.chatapp.chat.Chat_Activity
-import com.example.chatapp.databinding.ActivityNewsfeedBinding
+import com.example.chatapp.databinding.ActivityChatBinding
+import com.example.chatapp.newsfeed.NewsfeedActivity
 
-class NewsfeedActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityNewsfeedBinding
+class Chat_Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNewsfeedBinding.inflate(layoutInflater)
+        binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // Dùng để bottom navigation có thể chuyển fragment
-        navController = findNavController(R.id.fragmentContainerViewNews)
-        binding.bottomNavigationNews.setupWithNavController(navController)
+        navController = findNavController(R.id.fragmentContainerViewChat)
+        binding.bottomNavigationChat.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             // Cập nhật TextView dựa trên label của fragment
             binding.actionbarNewsName.text = destination.label
         }
-        //Chuyển qua chat
-        binding.imgChat.setOnClickListener {
-            startActivity(Intent(this, Chat_Activity::class.java))
+        //Chuyển về News Feed
+        binding.imgNews.setOnClickListener {
+            startActivity(Intent(this, NewsfeedActivity::class.java))
         }
     }
 }
